b271c020f89ae3464efceda38bcbc2465fa5afad
Kacper Mól <kacpermol450@gmail.com>
2024-03-27 10:27:33 +0100

    add first row content in MainWindow

--- zdjecia/MainWindow.xaml.cs
+++ zdjecia/MainWindow.xaml.cs
@@ -9,7 +9,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System;
+using System.Collections.Generic;
+using System.Windows.Media.Media3D;
 
 namespace zdjecia
 {
@@ -22,27 +24,9 @@ namespace zdjecia
         {
             InitializeComponent();
         }
-        private void OpenFolder_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void LoadImagesFromFolder(string folderPath)
-        {
-            ImageListView.Items.Clear();
-
-            List<BitmapImage> images = new List<BitmapImage>();
-            foreach (string imagePath in Directory.GetFiles(folderPath, "*.jpg"))
-            {
-                BitmapImage image = new BitmapImage(new Uri(imagePath));
-                images.Add(image);
-            }
-
-            ImageListView.ItemsSource = images;
-        }
         private void WybierzFolder_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
     }
 }

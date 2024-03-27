4ac114eba6cc3108d6ea902d8e145e3a0d100f7e
Kacper Mól <kacpermol450@gmail.com>
2024-03-27 10:09:01 +0100

    add userwindows forms in csproj

--- zdjecia/MainWindow.xaml.cs
+++ zdjecia/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 
 namespace zdjecia
 {
@@ -20,7 +22,24 @@ namespace zdjecia
         {
             InitializeComponent();
         }
+        private void OpenFolder_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
 
+        private void LoadImagesFromFolder(string folderPath)
+        {
+            ImageListView.Items.Clear();
+
+            List<BitmapImage> images = new List<BitmapImage>();
+            foreach (string imagePath in Directory.GetFiles(folderPath, "*.jpg"))
+            {
+                BitmapImage image = new BitmapImage(new Uri(imagePath));
+                images.Add(image);
+            }
+
+            ImageListView.ItemsSource = images;
+        }
         private void WybierzFolder_Click(object sender, RoutedEventArgs e)
         {
 
